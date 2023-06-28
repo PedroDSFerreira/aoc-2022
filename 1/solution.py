b8e66b1c4fcd8f06fcfea9690152cbf69c191446
Pedro Ferreira <pedrodsf21@gmail.com>
2023-06-28 14:36:15 +0100

    Complete challenge 1

---
+++ 1/solution.py
@@ -0,0 +1,14 @@
+def main():
+    elves = [0]
+    with open("input.txt") as file:
+        for line in file:
+            if line.strip():
+                elves[-1] = elves[-1] + int(line.strip())
+            else:
+                elves.append(0)
+    
+    print(max(elves))
+            
+
+if __name__ == "__main__":
+    main()
